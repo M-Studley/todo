32dbdb3b21a6b7a35218465bb92e347220a064f2
Simone <150595274+simonetimi@users.noreply.github.com>
2024-07-14 13:49:50 +0200

    refactor todo category as standalone component

---
+++ frontend/components/CategoryItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function CategoryItem({
+  title,
+  icon,
+}: {
+  title: string;
+  icon: React.ReactNode;
+}) {
+  return (
+    <div className="flex w-full items-center gap-3 rounded-xl bg-white/20 p-2 shadow-sm transition duration-300 hover:scale-105 hover:cursor-pointer">
+      {icon}
+      <p>{title}</p>
+    </div>
+  );
+}
--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -5,23 +5,22 @@ import {
   HomeIcon,
 } from '@heroicons/react/24/outline';
 
+import CategoryItem from './CategoryItem';
+
 export default function Sidebar() {
   return (
     <nav className="m-3rounded-xl row-span-7 border border-white border-opacity-20 bg-white bg-opacity-30 p-6 shadow-xl backdrop-blur-xl">
       <h1 className="mb-12 text-2xl">Todo App</h1>
       <section className="flex flex-col gap-4">
-        <div className="flex w-full items-center gap-3 rounded-xl bg-white/10 p-2 shadow-sm">
-          <HomeIcon width={22} />
-          <p>All</p>
-        </div>
-        <div className="flex w-full items-center gap-3 rounded-xl bg-white/10 p-2 shadow-sm">
-          <BuildingStorefrontIcon width={22} />
-          <p>Groceries</p>
-        </div>
-        <div className="flex w-full items-center gap-3 rounded-xl bg-white/10 p-2 shadow-sm">
-          <GlobeEuropeAfricaIcon width={22} />
-          <p>Travels</p>
-        </div>
+        <CategoryItem title="All" icon={<HomeIcon width={22} />} />
+        <CategoryItem
+          title="Groceries"
+          icon={<BuildingStorefrontIcon width={22} />}
+        />
+        <CategoryItem
+          title="Travels"
+          icon={<GlobeEuropeAfricaIcon width={22} />}
+        />
       </section>
     </nav>
   );
