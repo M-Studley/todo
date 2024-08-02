3bf4adde8907e53c280bc7596b287f7bda630292
Michael Studley <michael.r.studley@gmail.com>
2024-08-02 19:56:55 +0700

    CREATED:   - None UPDATED:   - routes.py DELETED:   - None

--- app/routes.py
+++ app/routes.py
@@ -1,38 +1,35 @@
-import json
-from flask import Blueprint, request, jsonify, redirect, url_for
+from flask import Blueprint, request, jsonify, make_response
 
 from .database import Database
 
 main = Blueprint('main', __name__)
 db = Database()
 
-
 @main.route('/todos', methods=['GET'])
 def get_all_todos():
     try:
         all_data = db.fetchall('SELECT * FROM todo')
         if all_data:
-            return json.dumps(all_data)
+            return jsonify(all_data)
+        else:
+            return make_response(jsonify([]), 204)
     except Exception as e:
         print(f'Error: {e}')
-
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)
 
 @main.route('/todos', methods=['POST'])
 def create_todo():
-    # Extracting json data
     data = request.get_json()
     if not data:
-        print('No data extracted...')
-    # Extracting keys and values
+        return make_response(jsonify({'error': 'No data provided'}), 400)
+
     columns = ', '.join(data.keys())
     values = tuple(data.values())
-    # Constructing query
     query = f"INSERT INTO todo ({columns}) VALUES ({', '.join(['%s'] * len(values))})"
-    # Executing query
+
     try:
         db.execute(query, values)
         return jsonify(data), 201
     except Exception as e:
         print(f'Error: {e}')
-
-    return redirect(url_for('get_all_todos'))
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500
