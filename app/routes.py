ae3b12be0505688545dc5c3179806db1da0518c2
Michael Studley <michael.r.studley@gmail.com>
2024-08-02 19:34:05 +0700

    CREATED:   - New model for todo UPDATED:   - get_all_todos with minor try except DELETED:   - Old test todo model

--- app/routes.py
+++ app/routes.py
@@ -9,19 +9,28 @@ db = Database()
 
 @main.route('/todos', methods=['GET'])
 def get_all_todos():
-    all_data = db.fetchall('SELECT * FROM test_todo')
-    return json.dumps(all_data)
+    try:
+        all_data = db.fetchall('SELECT * FROM test_todo')
+        if all_data:
+            return json.dumps(all_data)
+    except Exception as e:
+        print(f'Error: {e}')
 
 
 @main.route('/todos', methods=['POST'])
 def create_todo():
     # Extracting json data
     data = request.get_json()
+    if not data:
+        print('No data extracted...')
     # Extracting keys and values
     columns = ', '.join(data.keys())
     values = tuple(data.values())
     # Constructing query
     query = f"INSERT INTO test_todo ({columns}) VALUES ({', '.join(['%s'] * len(values))})"
     # Executing query
-    db.execute(query, values)
-    return jsonify(data), 201
+    try:
+        db.execute(query, values)
+        return jsonify(data), 201
+    except Exception as e:
+        print(f'Error: {e}')
