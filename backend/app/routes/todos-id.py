b254b958fcb93c09d9df6a7bd3ffeec929e8be5a
Michael Studley <michael.r.studley@gmail.com>
2024-08-09 13:34:06 +0700

    CREATED:   - None UPDATED:   - todos-id.py: update_todo_by_id DELETED:   - None

--- backend/app/routes/todos-id.py
+++ backend/app/routes/todos-id.py
@@ -20,8 +20,27 @@ def get_todo_by_id(db_id) -> flask.Response:
 
 
 @main.route('/todos/<int:db_id>', methods=['POST'])
-def update_todo_by_id(db_id):
-    # Create to-do object
-    todo_data = request.get_json()
-    todo = Todo(**todo_data)
-    ...
+def update_todo_by_id(db_id) -> flask.Response:
+    try:
+        # Create to-do object
+        todo_data = request.get_json()
+        todo = Todo(**todo_data)
+
+        # Validate to-do object
+        validate = TodoValidator()
+        if not validate.check_all(todo):
+            return make_response(jsonify({'error:': 'Invalid data provided!'}), 400)
+
+        # Prepare the update query
+        columns = ', '.join(f"{field.name} = %s" for field in fields(Todo))
+        values = tuple(getattr(todo, field.name) for field in fields(Todo)
+                       if getattr(todo, field.name) is not None) + (db_id,)
+        query = f"UPDATE todo SET {columns} WHERE id = %s"
+
+        # Execute the update query
+        db.execute(query, values)
+        return jsonify(todo_data)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return make_response(jsonify({'error': 'Internal Server Error!'}), 500)
