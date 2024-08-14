7c9a8523243378a034d1d7cc1429fafed3f1054a
Michael Studley <michael.r.studley@gmail.com>
2024-08-14 15:18:43 +0700

    CREATED:   - None UPDATED:   - DATABASE_URL DELETED:   - None

--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -5,6 +5,6 @@ class Config:
     # SET DATABASE URL FROM ENVIRONMENT VARIABLE
     DATABASE_URL = os.environ.get(
         'DATABASE_URL',
-        'postgresql://todo_99fq_user:UFzkR7Uld0Xn2s0JbwVrvTBKcIAHIw1P@dpg-cq8f2juehbks738imep0-a.oregon-postgres'
-        '.render.com/todo_99fq'
+        'postgresql://todo_3m8m_user:CBfySLlcCTMJJddARDWjJw3lWrWFWeak'
+        '@dpg-cqu60lbqf0us73a5sc80-a.oregon-postgres.render.com/todo_3m8m'
     )
