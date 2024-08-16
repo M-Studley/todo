c4f8cbac620c1660cf81846cae81be3297a303dd
Michael Studley <michael.r.studley@gmail.com>
2024-08-16 08:31:35 +0700

    CREATED:   - todos.py: delete_todo() UPDATED:   - todos.py: changed logic for update DELETED:   - None

--- backend/app/routes/todos.py
+++ backend/app/routes/todos.py
@@ -1,6 +1,5 @@
 import flask
 from flask import request, jsonify, make_response
-from dataclasses import fields
 
 from . import main, db
 from backend.app.models.models import Todo, TodoValidator
@@ -64,7 +63,7 @@ def get_todo_by_id(db_id) -> flask.Response:
         return make_response(jsonify({'error': 'Internal Server Error'}), 500)
 
 
-@main.route('/todos/<int:db_id>', methods=['POST'])
+@main.route('/todos/<int:db_id>', methods=['PATCH'])
 def update_todo_by_id(db_id) -> flask.Response:
     try:
         # Create to-do object
@@ -77,15 +76,15 @@ def update_todo_by_id(db_id) -> flask.Response:
             return make_response(jsonify({'error:': 'Invalid data provided!'}), 400)
 
         # Prepare the update query
-        columns = ', '.join(f"{field.name} = %s" for field in fields(todo))
-        values = tuple(getattr(todo, field.name) for field in fields(todo)
-                       if getattr(todo, field.name) is not None) + (db_id,)
-        query = f"UPDATE todo SET {columns} WHERE id = %s"
+        columns = list(key for key in todo_data.keys())
+        values = list(value for value in todo_data.values())
+        cols_vals = ", ".join(f'{cols} = {vals}' for cols, vals in zip(columns, values))
+        query = f"UPDATE todo SET {cols_vals} WHERE id = %s"
 
         # Execute the update query
         try:
-            db.execute(query, values)
-            return jsonify(todo_data)
+            db.execute(query, ...)
+            return jsonify(todo_data, (db_id,))
         except Exception as e:
             db.conn.rollback()
             print(f'Error: {e}')
@@ -94,3 +93,21 @@ def update_todo_by_id(db_id) -> flask.Response:
     except Exception as e:
         print(f"Error: {e}")
         return make_response(jsonify({'error': 'Internal Server Error!'}), 500)
+
+
+@main.route('/todos/<int:db_id>', methods=['DELETE'])
+def delete_todo(db_id):
+    try:
+        # Execute the delete query
+        result = db.execute('DELETE FROM todo WHERE id = %s RETURNING *', (db_id,))
+
+        # Check if a row was deleted
+        if result:
+            return make_response(jsonify({'message': 'Todo deleted successfully'}), 200)
+        else:
+            return make_response(jsonify({'error': 'Todo not found'}), 404)
+    except Exception as e:
+        print(f'Error: {e}')
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
+
