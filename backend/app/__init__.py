e2979541ce8cea29d8fb7f4a973421353f8ac462
Michael Studley <michael.r.studley@gmail.com>
2024-08-23 19:22:56 +0700

    CREATED:   - None UPDATED:   - app\__init__.py: debugging DELETED:   - None

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
-from backend.app.routes import todo_api, user_api
+from backend.app.routes.todos import todo_api
+# from backend.app.routes.users import user_api
 from backend.app.database.database import Database
 
 
@@ -9,12 +10,13 @@ def create_app():
     app = Flask(__name__)
 
     # Register blueprints
-    app.register_blueprint(blueprint=todo_api, url_prefix='/todos')
-    app.register_blueprint(blueprint=user_api, url_prefix='/users')
+    app.register_blueprint(todo_api, url_prefix='/todos')
+    # app.register_blueprint(user_api, url_prefix='/users')
 
-    # Initialize the Database
+    # Initialize Database
     Database.init()
 
+    print("Blueprints registered:")
     print(app.url_map)
 
     return app
--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 from backend.app.database.database import Database
 
 todo_api = Blueprint('todo_api', __name__)
-user_api = Blueprint('user_api', __name__)
+# user_api = Blueprint('user_api', __name__)
 db = Database()
